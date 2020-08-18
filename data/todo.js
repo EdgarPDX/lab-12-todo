6dd0ed871f4f760a1ffd0efbbd35e02ec2d4086f
EdgarPDX <66703437+EdgarPDX@users.noreply.github.com>
2020-08-18 14:08:21 -0700

    initial commit-changed data folder to fit data model

---
+++ data/todo.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    todo: 'wash the car',
+    completed: false,
+  },
+  {
+    todo: 'do the laundry',
+    completed: false,
+  },
+  {
+    todo: 'cook dinner',
+    completed: false,
+  },
+  {
+    todo: 'walk the dog',
+    completed: false,
+  }
+];
